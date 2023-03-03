0bd874e09c625992546da0738f5442b31981b890
Adrian Abeyta <77695369+abeyta@users.noreply.github.com>
2023-03-02 16:43:56 -0800

    Update motor_test.py

--- motor_test.py
+++ motor_test.py
@@ -1,10 +1,18 @@
 import RPi.GPIO as GPIO
 from time import sleep
 from datetime import datetime
+
 GPIO.setmode(GPIO.BOARD)
-clockwise = 11
-counter = 12
-Motor1A = clockwise
+
+# message to be imported from AWS
+message = 11
+close = 11
+open = 12
+if message = close:
+        Motor1A = close
+else:
+        Motor1A = open
+        
 GPIO.setup(Motor1A,GPIO.OUT)
 stop = False
 try: 
